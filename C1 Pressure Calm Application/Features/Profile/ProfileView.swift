771c0e1010d92db5554480bb2740e6e0f050f482
rea <jeanyaurellia@gmail.com>
2026-05-21 20:24:14 +0700

    fix profile

--- C1 Pressure Calm Application/Features/Profile/ProfileView.swift
+++ C1 Pressure Calm Application/Features/Profile/ProfileView.swift
@@ -13,56 +13,59 @@ struct ProfileView: View {
     
     var body: some View {
         NavigationStack{
-            VStack{
+            VStack(spacing: 0){
+
                 Text("Profile")
                     .font(.largeTitle)
                     .bold()
                     .padding(.horizontal)
                     .frame(maxWidth: .infinity, alignment: .leading)
-                Circle()
-                    .frame(width: 200, height: 200)
-                    .foregroundColor(.blue)
+                    .padding(.bottom, 60)
+
+                ZStack{
+                    Circle()
+                        .frame(width: 200, height: 200)
+                        .foregroundColor(.darkGreen)
+
+                    Image(systemName: "person")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .foregroundStyle(Color.lightGreen)
+                }
+
                 Text("User 1")
                     .font(.title)
+                    .bold()
+
                 NavigationLink{
                     ProfileDetail()
                 } label: {
                     Text("Edit foto")
                 }
-            }
-            
-            List{
-                Section(header: Text("Account")){
-                    NavigationLink{
-                        ProfileDetail()
-                    } label: {
-                        Text("Edit Profil")
-                    }
-                    
-                }
-                Section(header: Text("Personalisasi")){
-                    NavigationLink{
-                        PersonalisasiDetail()
-                    } label: {
-                        VStack{
-                            Text("Personalisasi")
+
+                List{
+                    Section{
+                        NavigationLink{
+                            ProfileDetail()
+                        } label: {
+                            Label("Manage Profil", systemImage: "gearshape")
+                        }
+
+                        NavigationLink{
+                            PersonalisasiDetail()
+                        } label: {
+                            Label("Personalisasi", systemImage: "slider.horizontal.3")
+                        }
+
+                        NavigationLink{
+
+                        } label: {
+                            Label("Manage Notification", systemImage: "bell")
                         }
                     }
-                    NavigationLink{
-                        
-                    } label: {
-                        VStack{
-                            Text("Take Test")
-                        }
-                    }
-                    
+                    .listRowBackground(Color.leafGreen)
                 }
-                Section(header: Text("Setting")){
-                    Toggle(
-                        "Notification",
-                        isOn: $vibrateOnRing
-                    )
-                }
+                .scrollContentBackground(.hidden)
             }
         }
         
